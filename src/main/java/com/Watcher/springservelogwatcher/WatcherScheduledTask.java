0d50600d3fbae153144da2fb8bd81d98cd22f53f
netanel96 <45965819+netanel96@users.noreply.github.com>
2022-04-21 15:05:01 +0300

    added code

--- src/main/java/com/Watcher/springservelogwatcher/WatcherScheduledTask.java
+++ src/main/java/com/Watcher/springservelogwatcher/WatcherScheduledTask.java
@@ -1,6 +1,7 @@
 package com.Watcher.springservelogwatcher;
 
 import app.Watcher;
+import candidateStocks.DBCandidateStocksFetcher;
 
 import java.io.FileNotFoundException;
 import java.util.TimerTask;
@@ -12,6 +13,8 @@ public class WatcherScheduledTask  extends TimerTask {
             Watcher watcher = new Watcher();
             System.out.println("watching..");
             watcher.watch();
+            DBCandidateStocksFetcher.mongoDBInterface.close();
+
         } catch (FileNotFoundException e) {
             System.out.println("some sheet happened");
             e.printStackTrace();
